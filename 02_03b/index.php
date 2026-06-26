e1a5a1285d7ad12bc6f3784f000d4a1f81a435f3
jerbypardogo <jerrell.pardo@go.team>
2026-06-26 05:40:11 +0000

    Hands-On PHP 02_03b:  file sorting helper and custom usort logic

--- 02_03b/index.php
+++ 02_03b/index.php
@@ -5,5 +5,23 @@ function print_array($a) {
 	echo '</pre>';
 }
 
+// Compare two file names by extension first, then by base filename.
+function compare_files($a, $b) {
+	$a = pathinfo($a);
+	$b = pathinfo($b);
+
+	// If the extensions match, sort by filename.
+	if (strcmp($a['extension'], $b['extension']) == 0) {
+		return strcmp($a['filename'], $b['filename']);
+	}
+
+	// Otherwise sort by extension.
+	strcmp($a['extension'], $b['extension']);
+}
+
 $dir = 'files';
 $files = scandir($dir);
+
+// Sort the file list using the custom compare function.
+usort($files, 'compare_files');
+print_array($files);
