c1d88b0594b5d6f9d88351fb459d567310027586
jerbypardogo <jerrell.pardo@go.team>
2026-06-26 05:22:28 +0000

    Hands-On PHP 02_02b:  scanning files and counting extensions

--- 02_02b/index.php
+++ 02_02b/index.php
@@ -7,3 +7,21 @@ function print_array($a) {
 }
 
 $dir = 'files';
+
+// Scan the directory for file entries.
+$files = scandir($dir);
+$extensions = array();
+
+foreach ($files as $file) {
+	// Skip directories and only count actual files.
+	if ( !is_dir($file) ) {
+		$ext = pathinfo($file, PATHINFO_EXTENSION);
+		if ( ! empty($ext) ) {
+			// Count each file extension occurrence.
+			$extensions[$ext] = ($extensions[$ext] ?? 0) + 1;
+		}
+	}
+}
+
+// Display the extension counts for the scanned files.
+print_array($extensions);
