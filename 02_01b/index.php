736133f28fe79279e0358c2ce8114810e181974c
jerbypardogo <jerrell.pardo@go.team>
2026-06-26 04:02:09 +0000

    Hands-On PHP 02_01b: add choice collection loop and random selection

--- 02_01b/index.php
+++ 02_01b/index.php
@@ -1,2 +1,15 @@
 <?php
 echo 'Create your list of choices: ' . "\n\n";
+
+// Collect user choices until the stop marker '0' is entered.
+$choices = array();
+
+do {
+  $choices[] = readline('Add a choice? (Type 0 to stop): ');
+} while ('0' !== end($choices));
+
+// Remove the terminating marker from the choices list.
+array_pop($choices);
+
+// Pick a random choice from the remaining options.
+echo 'And the deicsion is: ' . $choices[array_rand($choices)];
