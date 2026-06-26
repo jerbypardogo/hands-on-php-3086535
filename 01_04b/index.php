98d27e2937bbc03a0f1a2d96eaa60f424c757e92
jerbypardogo <jerrell.pardo@go.team>
2026-06-26 03:33:06 +0000

    Hands-On PHP 01_04b: add singleton connection comments and document reuse behavior

--- 01_04b/index.php
+++ 01_04b/index.php
@@ -4,14 +4,27 @@ class MyConnect {
 	private $username;
 	private $password;
 	private $host;
+
+	// Hold the singleton connection instance.
+	private static $connection;
 	
-	function __construct($name, $username, $password) {
+	// Make the constructor private so instances can only be created through open_connection().
+	private function __construct($name, $username, $password) {
 		$this->db_name = $name;
 		$this->username = $username;
 		$this->password = $password;
 		$this->host = 'localhost';	
 	}
 	
+	// Return the existing connection or create it once if needed.
+	public static function open_connection($name, $username, $password) {
+		if ( !isset( self::$connection ) ) {
+			self::$connection = new MyConnect( $name, $username, $password );
+		}
+
+		return self::$connection;
+	}
+
 	public function get_info( $query ) {
 		//STUB
 	}
@@ -25,5 +38,10 @@ class MyConnect {
 	}
 }
 
-$db = new MyConnect( 'people', 'joe', 'hello_there' );
+// Open a single shared connection to the database.
+$db = MyConnect::open_connection( 'people', 'joe', 'hello_there' );
+echo $db->get_db_name() . '<br />';
+
+// Reuse the same connection instance even with a second call.
+$db2 = MyConnect::open_connection( 'place', 'joe', 'hello_there' );
 echo $db->get_db_name();
